f3bdc7ebd29c79a9cb3ee9ee86c4d9a3f0b18c0b
Alok Kumar Mishra <alok99893@gmail.com>
2023-04-23 00:51:36 +0530

    updated

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const users = require("./models/userSchema");
 const cors = require("cors");
 const router = require("./routes/router")
 
-const port = process.env.PORT || 8003;
+const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 app.use(router);
